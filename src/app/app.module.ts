ba606f322dfbad5d60ee03e2e59d9fad59a991a3
Bryan <bryanvargasf2110@gmail.com>
2019-03-19 21:15:08 -0600

    Landingpage creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './public/homepage/homepage.component';
+import { LandingpageComponent } from './public/landingpage/landingpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule,

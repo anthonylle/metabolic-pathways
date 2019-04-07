9f1828cc82da7adbb3759f5b640a1ab85c756444
Daniel Delgado <delgado.jdds@gmail.com>
2019-04-06 21:00:45 -0600

    A1T1-A1T5 working

--- python/AlgorithmsFunctions.py
+++ python/AlgorithmsFunctions.py
@@ -4,7 +4,7 @@ from AuxiliaryFunctions import *
 
 # Code: C1
 def createOneCompoundGraph(imagesFolderPath, tempUploadsFolderPath, XMLFileName1):
-    output = {}
+    output = {"Algorithm": "C1"}
     simpleKGMLStart1 = SimpleKGML(tempUploadsFolderPath + XMLFileName1)
     simpleKGML1 = SimpleKGML(tempUploadsFolderPath + XMLFileName1)
     pathwayCompoundsGraph1 = simpleKGML1.getCompoundsGraph()
@@ -17,14 +17,14 @@ def createOneCompoundGraph(imagesFolderPath, tempUploadsFolderPath, XMLFileName1
 
 # Code: C2
 def createTwoCompoundGraphs(imagesFolderPath, tempUploadsFolderPath, XMLFileName1, XMLFileName2):
-    output = {}
+    output = {"Algorithm": "C2"}
     output["Compound Graph 1"] = createOneCompoundGraph(imagesFolderPath, tempUploadsFolderPath, XMLFileName1)["Compound Graph 1"]
     output["Compound Graph 2"] = createOneCompoundGraph(imagesFolderPath, tempUploadsFolderPath, XMLFileName2)["Compound Graph 1"]
     return output
 
 # Code: S1
 def createOneCentralNodeGraph(imagesFolderPath, tempUploadsFolderPath, XMLFileName1):
-    output = {}
+    output = {"Algorithm": "S1"}
     simpleKGMLStart1 = SimpleKGML(tempUploadsFolderPath + XMLFileName1)
     simpleKGML1 = SimpleKGML(tempUploadsFolderPath + XMLFileName1)
     centralNodeGraph1 = simpleKGML1.getCentralNodeGraph()
@@ -37,9 +37,29 @@ def createOneCentralNodeGraph(imagesFolderPath, tempUploadsFolderPath, XMLFileNa
 
 # Code: S2
 def createTwoCentralNodeGraphs(imagesFolderPath, tempUploadsFolderPath, XMLFileName1, XMLFileName2):
-    output = {}
+    output = {"Algorithm": "S2"}
     output["Central Node Graph 1"] = createOneCentralNodeGraph(imagesFolderPath, tempUploadsFolderPath, XMLFileName1)["Central Node Graph 1"]
     output["Central Node Graph 2"] = createOneCentralNodeGraph(imagesFolderPath, tempUploadsFolderPath, XMLFileName2)["Central Node Graph 1"]
+    return output
+
+# Code: NIndex
+def getGraphNodesIndexes(pathwayGraph):
+    output = {"Algorithm": "NIndex", "Nodes indexes": {}}
+    graph = to_graph_from_dict(pathwayGraph)
+    for n, i in zip(range(len(graph.get_nodes())), graph.get_nodes()):
+        output["Nodes indexes"][n] = i.get_value()
+    return output
+
+# Code: GPaths
+def getGraphPathsIndexes(pathwayGraph, startNodeGraph, endNodeGraph, maximumCycles):
+    output = {"Algorithm": "GPaths", "Graph paths": {}}
+    graph = to_graph_from_dict(pathwayGraph)
+    paths = graph.get_cyclic_paths(graph[int(startNodeGraph)], graph[int(endNodeGraph)], [], int(maximumCycles))
+    for n, i in zip(range(len(paths)), paths):
+        temporalPath = []
+        for node in i:
+            temporalPath.append(node.get_value())
+        output["Graph paths"][n] = temporalPath
     return output
 
 # Code: A1T
@@ -48,7 +68,7 @@ def alg1Transformation2DtoVector(imagesFolderPath, tempUploadsFolderPath, pathwa
 
 # Code: A1T1
 def alg1_1GraphTraversal_AnyNodeToAnyNode(imagesFolderPath, tempUploadsFolderPath, pathwayGraph1, pathwayGraph2):
-    output = {}
+    output = {"Algorithm": "A1T1"}
     graph1 = to_graph_from_dict(pathwayGraph1)
     graph2 = to_graph_from_dict(pathwayGraph2)
     bft1 = graph1.breadth_first_traversal()
@@ -77,13 +97,13 @@ def alg1_1GraphTraversal_AnyNodeToAnyNode(imagesFolderPath, tempUploadsFolderPat
 
 # Code: A1T2
 def alg1_2GraphTraversal_GivenNodeToAnyNode(imagesFolderPath, tempUploadsFolderPath, pathwayGraph1, pathwayGraph2, startNodeGraph1, startNodeGraph2):
-    output = {}
+    output = {"Algorithm": "A1T2"}
     graph1 = to_graph_from_dict(pathwayGraph1)
     graph2 = to_graph_from_dict(pathwayGraph2)
-    bft1 = graph1.breadth_first_traversal()
-    bft2 = graph2.breadth_first_traversal()
-    dft1 = graph1.depth_first_traversal()
-    dft2 = graph2.depth_first_traversal()
+    bft1 = graph1.breadth_first_traversal(int(startNodeGraph1))
+    bft2 = graph2.breadth_first_traversal(int(startNodeGraph2))
+    dft1 = graph1.depth_first_traversal(int(startNodeGraph1))
+    dft2 = graph2.depth_first_traversal(int(startNodeGraph2))
     addToDictionary(bft1)
     addToDictionary(bft2)
     addToDictionary(dft1)
@@ -105,16 +125,92 @@ def alg1_2GraphTraversal_GivenNodeToAnyNode(imagesFolderPath, tempUploadsFolderP
     return output
 
 # Code: A1T3
-def alg1_3GraphTraversal_GivenNodeToGivenNode(imagesFolderPath, tempUploadsFolderPath, XMLFileName1, XMLFileName2):
-    return "Ok"
+def alg1_3GraphTraversal_GivenNodeToGivenNode(imagesFolderPath, tempUploadsFolderPath, pathwayGraph1, pathwayGraph2, startNodeGraph1, startNodeGraph2, endNodeGraph1, endNodeGraph2):
+    output = {"Algorithm": "A1T3"}
+    graph1 = to_graph_from_dict(pathwayGraph1)
+    graph2 = to_graph_from_dict(pathwayGraph2)
+    bft1 = graph1.breadth_first_traversal(int(startNodeGraph1), int(endNodeGraph1))
+    bft2 = graph2.breadth_first_traversal(int(startNodeGraph2), int(endNodeGraph2))
+    dft1 = graph1.depth_first_traversal(int(startNodeGraph1), int(endNodeGraph1))
+    dft2 = graph2.depth_first_traversal(int(startNodeGraph2), int(endNodeGraph2))
+    addToDictionary(bft1)
+    addToDictionary(bft2)
+    addToDictionary(dft1)
+    addToDictionary(dft2)
+
+    output["BFT1"] = renamedPath(bft1)
+    output["BFT2"] = renamedPath(bft2)
+    output["DFT1"] = renamedPath(dft1)
+    output["DFT2"] = renamedPath(dft2)
+    output["Global BFT"] = needleman_wunsch(renamedPath(bft1), renamedPath(bft2))
+    output["Global DFT"] = needleman_wunsch(renamedPath(dft1), renamedPath(dft2))
+    output["Local BFT"] = local_alignment(renamedPath(bft1), renamedPath(bft2))
+    output["Local DFT"] = local_alignment(renamedPath(dft1), renamedPath(dft2))
+    output["SemiLocal BFT"] = semiglobal_alignment(renamedPath(bft1), renamedPath(bft2))
+    output["SemiLocal DFT"] = semiglobal_alignment(renamedPath(dft1), renamedPath(dft2))
+    output["Differences 1-2"] = identify_differences(graph1, graph2, FULL)
+    output["Differences 2-1"] = identify_differences(graph2, graph1, FULL)
+
+    return output
 
 # Code: A1T4
-def alg1_4EvalPossiblePaths_GivenNodeToGivenNode(imagesFolderPath, tempUploadsFolderPath, XMLFileName1, XMLFileName2):
-    return "Ok"
+def alg1_4EvalPossiblePaths_GivenNodeToGivenNode(imagesFolderPath, tempUploadsFolderPath, pathwayGraph1, pathwayGraph2, selectedPath1, selectedPath2):
+    output = {"Algorithm": "A1T4"}
+    graph1 = to_graph_from_dict(pathwayGraph1)
+    graph2 = to_graph_from_dict(pathwayGraph2)
+
+    bft1 = graph1.breadth_first_traversal(int(selectedPath1))
+    bft2 = graph2.breadth_first_traversal(int(selectedPath2))
+    dft1 = graph1.depth_first_traversal(int(selectedPath1))
+    dft2 = graph2.depth_first_traversal(int(selectedPath2))
+    addToDictionary(bft1)
+    addToDictionary(bft2)
+    addToDictionary(dft1)
+    addToDictionary(dft2)
+
+    output["BFT1"] = renamedPath(bft1)
+    output["BFT2"] = renamedPath(bft2)
+    output["DFT1"] = renamedPath(dft1)
+    output["DFT2"] = renamedPath(dft2)
+    output["Global BFT"] = needleman_wunsch(renamedPath(bft1), renamedPath(bft2))
+    output["Global DFT"] = needleman_wunsch(renamedPath(dft1), renamedPath(dft2))
+    output["Local BFT"] = local_alignment(renamedPath(bft1), renamedPath(bft2))
+    output["Local DFT"] = local_alignment(renamedPath(dft1), renamedPath(dft2))
+    output["SemiLocal BFT"] = semiglobal_alignment(renamedPath(bft1), renamedPath(bft2))
+    output["SemiLocal DFT"] = semiglobal_alignment(renamedPath(dft1), renamedPath(dft2))
+    output["Differences 1-2"] = identify_differences(graph1, graph2, FULL)
+    output["Differences 2-1"] = identify_differences(graph2, graph1, FULL)
+
+    return output
 
 # Code: A1T5
-def alg1_5GraphTraversal_AnyNodeToGivenNode(imagesFolderPath, tempUploadsFolderPath, XMLFileName1, XMLFileName2):
-    return "Ok"
+def alg1_5GraphTraversal_AnyNodeToGivenNode(imagesFolderPath, tempUploadsFolderPath, pathwayGraph1, pathwayGraph2, endNodeGraph1, endNodeGraph2):
+    output = {"Algorithm": "A1T5"}
+    graph1 = to_graph_from_dict(pathwayGraph1)
+    graph2 = to_graph_from_dict(pathwayGraph2)
+    bft1 = graph1.breadth_first_traversal(endNode = int(endNodeGraph1))
+    bft2 = graph2.breadth_first_traversal(endNode = int(endNodeGraph2))
+    dft1 = graph1.depth_first_traversal(endNode = int(endNodeGraph1))
+    dft2 = graph2.depth_first_traversal(endNode = int(endNodeGraph2))
+    addToDictionary(bft1)
+    addToDictionary(bft2)
+    addToDictionary(dft1)
+    addToDictionary(dft2)
+
+    output["BFT1"] = renamedPath(bft1)
+    output["BFT2"] = renamedPath(bft2)
+    output["DFT1"] = renamedPath(dft1)
+    output["DFT2"] = renamedPath(dft2)
+    output["Global BFT"] = needleman_wunsch(renamedPath(bft1), renamedPath(bft2))
+    output["Global DFT"] = needleman_wunsch(renamedPath(dft1), renamedPath(dft2))
+    output["Local BFT"] = local_alignment(renamedPath(bft1), renamedPath(bft2))
+    output["Local DFT"] = local_alignment(renamedPath(dft1), renamedPath(dft2))
+    output["SemiLocal BFT"] = semiglobal_alignment(renamedPath(bft1), renamedPath(bft2))
+    output["SemiLocal DFT"] = semiglobal_alignment(renamedPath(dft1), renamedPath(dft2))
+    output["Differences 1-2"] = identify_differences(graph1, graph2, FULL)
+    output["Differences 2-1"] = identify_differences(graph2, graph1, FULL)
+
+    return output
 
 # Code: A2
 def alg2DifferentiationByPairs(imagesFolderPath, tempUploadsFolderPath, XMLFileName1, XMLFileName2):
--- python/Main.py
+++ python/Main.py
@@ -7,11 +7,13 @@ algorithmTypes = {
     "C2": [4, createTwoCompoundGraphs],
     "S1": [3, createOneCentralNodeGraph],
     "S2": [4, createTwoCentralNodeGraphs],
+    "NIndex": [3, getGraphNodesIndexes],
+    "GPaths": [6, getGraphPathsIndexes],
     "A1T": [4, alg1Transformation2DtoVector],
     "A1T1": [4, alg1_1GraphTraversal_AnyNodeToAnyNode],
     "A1T2": [6, alg1_2GraphTraversal_GivenNodeToAnyNode],
     "A1T3": [8, alg1_3GraphTraversal_GivenNodeToGivenNode],
-    "A1T4": [8, alg1_4EvalPossiblePaths_GivenNodeToGivenNode],
+    "A1T4": [6, alg1_4EvalPossiblePaths_GivenNodeToGivenNode],
     "A1T5": [6, alg1_5GraphTraversal_AnyNodeToGivenNode],
     "A2": [4, alg2DifferentiationByPairs],
     "A3": [0, alg3_NameTBD]
@@ -32,20 +34,32 @@ if __name__ == '__main__':
 
         if len(sys.argv) == parametersNumber:
             if parametersNumber == 3:
-                print(functionToCall(imagesFolderPath, tempUploadsFolderPath,
-                                    sys.argv[1]))
+                if sys.argv[-1] == "NIndex":
+                    print(functionToCall(sys.argv[1]))
+                else:
+                    print(functionToCall(imagesFolderPath, tempUploadsFolderPath,
+                                        sys.argv[1]))
             elif parametersNumber == 4:
                 print(functionToCall(imagesFolderPath, tempUploadsFolderPath,
                                     sys.argv[1], sys.argv[2]))
             elif parametersNumber == 6:
-                print(functionToCall(imagesFolderPath, tempUploadsFolderPath,
-                                    sys.argv[1], sys.argv[2],
-                                    sys.argv[3], sys.argv[4]))
+                if sys.argv[-1] == "GPaths":
+                    print(functionToCall(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]))
+                else:
+                    print(functionToCall(imagesFolderPath, tempUploadsFolderPath,
+                                        sys.argv[1], sys.argv[2],
+                                        sys.argv[3], sys.argv[4]))
             elif parametersNumber == 8:
                 print(functionToCall(imagesFolderPath, tempUploadsFolderPath,
                                     sys.argv[1], sys.argv[2],
                                     sys.argv[3], sys.argv[4],
                                     sys.argv[5], sys.argv[6]))
+            elif parametersNumber == 9:
+                print(functionToCall(imagesFolderPath, tempUploadsFolderPath,
+                                    sys.argv[1], sys.argv[2],
+                                    sys.argv[3], sys.argv[4],
+                                    sys.argv[5], sys.argv[6],
+                                    sys.argv[7]))
         else:
             print("ERROR: incorrect number of parameters.")
     else:

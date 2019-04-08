330631ebcd814a762be8a51809dddd7c6b99f1f8
Anthony Leandro <aleandro@git.ec.tec.ac.cr>
2019-04-07 18:48:27 -0600

    Start & Finish Nodes selectors added

--- src/app/public/homepage/algorithm-selector/algorithm-selector.component.ts
+++ src/app/public/homepage/algorithm-selector/algorithm-selector.component.ts
@@ -33,7 +33,11 @@ export class AlgorithmSelectorComponent implements OnInit {
       this.options = [{value: 'Ori1', label:'2D to 1D Transformation'}, {value: 'Ori2', label:'Differentiation by pairs'}];
     }else{
       if (currentView === 'Extended'){
-        this.options = [{value: '1.1', label: '1.1'},{value: '1.2', label: '1.2'},{value: '1.3', label: '1.3'},{value: '1.4', label: '1.4'},{value: '1.5', label: '1.5'}];
+        this.options = [{value: '1.1', label: '1.1 - Starting at any node, Finishing at any node'},
+                        {value: '1.2', label: '1.2 - Starting at a given node, Finishing at any node'},
+                        {value: '1.3', label: '1.3 - Starting at a given node, Finishing at a given node'},
+                        {value: '1.4', label: '1.4 - Evaluation of all possible paths'},
+                        {value: '1.5', label: '1.5 - Starting at any node, Finishing at a given node'}];
       }else{
         this.options = [{value: '0', label: "Not available yet"}];
       }
